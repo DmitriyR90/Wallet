60be7a60fea70a6a85e949447ebb42433d3becf4
Andrey-Shtanko <shtankoandrew90@gmail.com>
2022-10-16 11:33:14 +0300

    add-hooks-on-form

--- src/components/TransactionForm/TransactionForm.jsx
+++ src/components/TransactionForm/TransactionForm.jsx
@@ -2,96 +2,97 @@ import { Form, ButtonForm } from './TransactionForm.styled';
 import Container from './../Container/Container';
 import { Section } from './../Section/Section';
 import { LabelInput } from 'components/LabelInput/LabelInput';
-import { Component } from 'react';
+import { useState } from 'react';
+const initialState = {
+  date: '',
+  time: '',
+  summ: '',
+  currency: 'UAH',
+  comment: '',
+  transactionType: 'expense',
+};
 
-export class TransactionForm extends Component {
-  state = {
-    date: '',
-    time: '',
-    summ: '',
-    currency: 'UAH',
-    comment: '',
-    transactionType: 'expense',
-  };
+export const TransactionForm = ({ addTransaction }) => {
+  const [form, setForm] = useState(initialState);
 
-  handleInput = ({ currentTarget }) => {
-    const { setState } = this;
-
+  const handleInput = ({ currentTarget }) => {
     const { name, value } = currentTarget;
-
-    setState.call(this, { [name]: value });
+    setForm(prevState => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
   };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
 
-    this.props.addTransaction(this.state);
+    addTransaction(form);
   };
 
-  render() {
-    const { date, time, summ, currency, comment, transactionType } = this.state;
+  const { date, time, summ, currency, comment, transactionType } = form;
 
-    return (
-      <Section>
-        <Container>
-          <Form onSubmit={this.handleSubmit}>
-            <LabelInput
-              title={'Дохід'}
-              type={'radio'}
-              name={'transactionType'}
-              value={'income'}
-              onInput={this.handleInput}
-              checked={'income' === transactionType}
-            />
-            <LabelInput
-              title={'Витрати'}
-              type={'radio'}
-              name={'transactionType'}
-              value={'expense'}
-              onInput={this.handleInput}
-              checked={'expense' === transactionType}
-            />
-            <LabelInput
-              title={'Дата'}
-              type={'date'}
-              name={'date'}
-              value={date}
-              onInput={this.handleInput}
-            />
-            <LabelInput
-              title={'Час'}
-              type={'time'}
-              name={'time'}
-              value={time}
-              onInput={this.handleInput}
-            />
-            <LabelInput
-              title={'Сума'}
-              type={'text'}
-              name={'summ'}
-              placeholder="Вкажіть суму"
-              value={summ}
-              onInput={this.handleInput}
-            />
-            <LabelInput
-              title={'Валюта'}
-              type={'button'}
-              name={'currency'}
-              value={currency}
-              onInput={this.handleInput}
-            />
-            <LabelInput
-              title={'Коментар'}
-              type={'text'}
-              name={'comment'}
-              placeholder="Коментар..."
-              value={comment}
-              onInput={this.handleInput}
-            />
-            <ButtonForm type="submit">Додати</ButtonForm>
-          </Form>
-        </Container>
-      </Section>
-    );
-  }
-}
+  return (
+    <Section>
+      <Container>
+        <Form onSubmit={handleSubmit}>
+          <LabelInput
+            title={'Дохід'}
+            type={'radio'}
+            name={'transactionType'}
+            value={'income'}
+            onInput={handleInput}
+            checked={'income' === transactionType}
+          />
+          <LabelInput
+            title={'Витрати'}
+            type={'radio'}
+            name={'transactionType'}
+            value={'expense'}
+            onInput={this.handleInput}
+            checked={'expense' === transactionType}
+          />
+          <LabelInput
+            title={'Дата'}
+            type={'date'}
+            name={'date'}
+            value={date}
+            onInput={this.handleInput}
+          />
+          <LabelInput
+            title={'Час'}
+            type={'time'}
+            name={'time'}
+            value={time}
+            onInput={this.handleInput}
+          />
+          <LabelInput
+            title={'Сума'}
+            type={'text'}
+            name={'summ'}
+            placeholder="Вкажіть суму"
+            value={summ}
+            onInput={this.handleInput}
+          />
+          <LabelInput
+            title={'Валюта'}
+            type={'button'}
+            name={'currency'}
+            value={currency}
+            onInput={this.handleInput}
+          />
+          <LabelInput
+            title={'Коментар'}
+            type={'text'}
+            name={'comment'}
+            placeholder="Коментар..."
+            value={comment}
+            onInput={this.handleInput}
+          />
+          <ButtonForm type="submit">Додати</ButtonForm>
+        </Form>
+      </Container>
+    </Section>
+  );
+};
